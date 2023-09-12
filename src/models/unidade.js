9ad014678b25837132d6f7fa83b4599031bd8447
wgeovanni <willyangeo@gmail.com>
2023-09-11 22:39:51 -0300

    feat: Implementação de validações na model unidades

--- src/models/unidade.js
+++ src/models/unidade.js
@@ -4,18 +4,42 @@ const { STRING, INTEGER, ENUM, DATE } = require('sequelize')
 const Unidade = connection.define('Unidade', {
     name: {
         type: STRING,
+        validate: {
+            len: {
+                args: [2, 30],
+                msg: "O campo deve conter entre 2 e 30 caracteres"
+            }
+        },
         allowNull: false
     },
     address: {
         type: STRING,
+        validate: {
+            len: {
+                args: [2, 30],
+                msg: "O campo deve conter entre 2 e 30 caracteres"
+            }
+        },
         allowNull: false
     },
     brand: {
         type: STRING,
+        validate: {
+            len: {
+                args: [2, 30],
+                msg: "O campo deve conter entre 2 e 30 caracteres"
+            }
+        },
         allowNull: false
     },
     model: {
         type: STRING,
+        validate: {
+            len: {
+                args: [2, 30],
+                msg: "O campo deve conter entre 2 e 30 caracteres"
+            }
+        },
         allowNull: false
     },
     active: {
