af818c94d8660c67ae6575281ca823c9dda32d83
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2023-09-11 22:46:02 -0300

    feat: add migration and generation.js

---
+++ src/models/generation.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+const { connection } = require('../database/connection');
+
+const Generation = connection.define("Generation", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  reference_date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  total_generated: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+},
+{
+  underscored: true,
+  paranoid: true,
+}
+);
+
+Generation.belongsTo(Unidade, {
+  foreignKey: 'idUnidade',
+});
+
+module.exports = {
+  Generation
+};
