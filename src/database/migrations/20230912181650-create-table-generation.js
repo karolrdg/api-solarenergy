71738c072c103135e89e6b8aed5c0ce1d89d38f7
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2023-09-12 15:22:25 -0300

    fix: migrations table generation

---
+++ src/database/migrations/20230912181650-create-table-generation.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("generations", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      reference_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      total_generated: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: Sequelize.DATE,
+      updated_at: Sequelize.DATE,
+      deleted_at: Sequelize.DATE,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("generations");
+  },
+};
