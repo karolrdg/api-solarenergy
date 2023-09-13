92692c5294250c91557094215e5d68c3896a7698
Erico Couto Jr <erico.coutjr@gmail.com>
2023-09-13 20:50:18 -0300

    fix: rota com id do listGeneration

--- src/routes/generation.routes.js
+++ src/routes/generation.routes.js
@@ -9,7 +9,7 @@ const { auth } = require("../midlleware/auth")
 class GenerationRouter {
   routesFromGeneration() {
     const generationRoutes = Router();
-    generationRoutes.get("/v1/geracao", auth, listGeneration);
+    generationRoutes.get("/v1/geracao/:id", auth, listGeneration);
     generationRoutes.post("/v1/geracao", auth, createGeneration);
     generationRoutes.put("/v1/geracao/:id", auth, editGeneration);
     generationRoutes.delete("/v1/geracao/:id", auth, deleteGeneration);
