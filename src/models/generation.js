d0594ec3c207c78ae97c2082c6b7a1f1ce1cb3cf
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2023-09-13 20:23:20 -0300

    fix: model e migration [generation]

--- src/models/generation.js
+++ src/models/generation.js
@@ -1,32 +1,44 @@
-const { DataTypes } = require('sequelize');
-const { connection } = require('../database/connection');
-const { Unidade } = require('./unidade');
+const { DataTypes } = require("sequelize");
+const { connection } = require("../database/connection");
+const { Unidade } = require("./unidade");
 
-const Generation = connection.define("Generation", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+const Generation = connection.define(
+  "Generation",
+  {
+    created_by: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: {
+          tableName: "unidades",
+        },
+        key: "id",
+      },
+    },
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    reference_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    total_generated: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  reference_date: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  total_generated: {
-    type: DataTypes.STRING,
-    allowNull: false,
+  {
+    underscored: true,
+    paranoid: true,
   }
-},
-{
-  underscored: true,
-  paranoid: true,
-}
 );
 
 Generation.belongsTo(Unidade, {
-  foreignKey: 'idUnidade',
+  foreignKey: "created_by",
 });
 
 module.exports = {
-  Generation
+  Generation,
 };
